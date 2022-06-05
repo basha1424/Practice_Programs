e1e933567a3d33bf3508ee040f67ef2e307884b3
mahaboob <bashaprnc12@gmail.com>
2022-06-05 11:07:43 +0530

    Added: SomeText

--- src/com/practies/Factorial.java
+++ src/com/practies/Factorial.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	
 		int fact=1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the  value");
 		int n = sc.nextInt();
-//      System.out.println("enter the ending value");
+      System.out.println("factorial value of "+n);
 //     int  end= sc.nextInt();
 		for (int i =n; i > 0; i--) {
 
